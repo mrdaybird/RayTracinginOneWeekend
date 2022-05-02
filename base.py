93c55f7736ee92e4ada5a5df018039f69031c642
Vaibhav Pathak <pathakvaibhav@protonmail.com>
2022-05-03 00:32:04 +0530

    Updated the code till Chapter 5

--- base.py
+++ base.py
@@ -14,14 +14,25 @@ viewport_width = ASPECT_RATIO * viewport_height
 focal_length = 1
 
 origin = np.array([0,0,-1])
+def hit_sphere(center, radius, origins:np.ndarray, directions:np.ndarray) -> np.ndarray:
+    oc = origins - center
+    a = np.sum(directions * directions, axis=1)
+    b = 2 * np.sum(oc * directions, axis=1)
+    c = np.sum(oc * oc, axis=1) - radius*radius
+    discriminant = b*b - 4*a*c
+    return discriminant > 0
 
 def normalize(a : np.ndarray) -> np.ndarray:
     return ((1/np.linalg.norm(a, axis=1)) * a.T).T
-def ray_color(a : np.ndarray) -> np.ndarray:
-    unit_direction = normalize(a)
+def ray_color(origins : np.ndarray, directions : np.ndarray) -> np.ndarray:
+    unit_direction = normalize(directions)
     t_array = np.multiply(np.add(unit_direction[:, 1],1),0.5)
     colors = ((1-t_array)*np.tile([1.0,1.0,1.0], WIDTH * HEIGHT).reshape(-1, 3).T + 
                 (t_array * np.tile([0.5,0.7,1.0], WIDTH*HEIGHT).reshape(-1, 3).T)).T
+
+    hits = hit_sphere(np.array([0,0,-1]), 0.5, origins, directions)
+    hits = np.repeat(hits, 3).reshape(-1, 3)
+    colors = (1-hits)*colors + hits*np.tile([1,0,0], 90000).reshape(90000, 3)
     return colors
 def rgb_color(a : np.ndarray) -> np.ndarray:
     '''
@@ -35,8 +46,9 @@ x = np.linspace(-viewport_width/2, viewport_width/2, WIDTH)
 y = np.linspace(-viewport_height/2, viewport_height/2, HEIGHT)
 screen = np.stack([np.tile(x, HEIGHT), np.repeat(y, WIDTH), np.zeros(WIDTH * HEIGHT)]).T
 
+origin_array = np.zeros((90000, 3))
 direction_array = screen - origin
-colors = ray_color(direction_array)
+colors = ray_color(origin_array, direction_array)
 
 image_array = colors.reshape(HEIGHT, WIDTH, 3)
 
